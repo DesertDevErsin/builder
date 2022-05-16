71020c94139c7d72a75387623a6623bc2fae2b7d
Ersin Akinci <me@ersinakinci.com>
2022-05-16 09:29:08 -0700

    refactor node-express example

--- examples/integrating-models/node-express/data.js
+++ examples/integrating-models/node-express/data.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const app = express();
 // TODO: Replace with your Builder public API key
-const apiKey = '77ef16bfb97145249d018edf89ba2eb0';
+const apiKey = 'YOUR_API_KEY';
 const port = 3000;
 
 const handleError = err => {
@@ -17,19 +17,11 @@ const handleError = err => {
 // Catchall route
 app.get('*', async (req, res) => {
   const encodedUrl = encodeURIComponent(req.url);
-  const { data: headerData } = await axios
-    .get(`https://cdn.builder.io/api/v1/html/section?apiKey=${apiKey}&userAttributes.name=Header`)
-    .catch(handleError);
-  const { data: pageData } = await axios
-    .get(`https://cdn.builder.io/api/v1/html/page?apiKey=${apiKey}&url=${encodedUrl}`)
-    .catch(handleError);
   const { data: pricingData } = await axios
     .get(`https://cdn.builder.io/api/v2/content/pricing?apiKey=${apiKey}&sort.data.price=1`)
     .catch(handleError);
 
-  if (headerData && pageData && pricingData) {
-    const headerHtml = headerData.data.html;
-    const pageHtml = pageData.data.html;
+  if (pricingData) {
     const pricingHtml = pricingData.results
       .map(result => {
         const name = result.name;
@@ -44,8 +36,6 @@ app.get('*', async (req, res) => {
       <html>
         <head> <!-- Your head content here --> </head>
         <body>
-           ${headerHtml}
-           ${pageHtml}
            ${pricingHtml}
         </body>
       </html>
--- examples/integrating-models/node-express/section.js
+++ examples/integrating-models/node-express/section.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const app = express();
 // TODO: Replace with your Builder public API key
-const apiKey = '77ef16bfb97145249d018edf89ba2eb0';
+const apiKey = 'YOUR_API_KEY';
 const port = 3000;
 
 const handleError = err => {
@@ -14,39 +14,23 @@ const handleError = err => {
   throw err;
 };
 
-// Catchall route
 app.get('*', async (req, res) => {
   const encodedUrl = encodeURIComponent(req.url);
   const { data: headerData } = await axios
     .get(`https://cdn.builder.io/api/v1/html/section?apiKey=${apiKey}&userAttributes.name=Header`)
     .catch(handleError);
-  const { data: pageData } = await axios
-    .get(`https://cdn.builder.io/api/v1/html/page?apiKey=${apiKey}&url=${encodedUrl}`)
-    .catch(handleError);
-  const { data: pricingData } = await axios
-    .get(`https://cdn.builder.io/api/v2/content/pricing?apiKey=${apiKey}&sort.data.price=1`)
-    .catch(handleError);
 
-  if (headerData && pageData && pricingData) {
+  if (headerData ) {
     const headerHtml = headerData.data.html;
-    const pageHtml = pageData.data.html;
-    const pricingHtml = pricingData.results
-      .map(result => {
-        const name = result.name;
-        const formatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
-        const price = formatter.format(result.data.price);
-
-        return `<div style="text-align: center;">${name}: ${price}</div>`;
-      })
-      .join('');
 
     res.send(`
       <html>
         <head> <!-- Your head content here --> </head>
         <body>
+           <!-- Your header here -->
            ${headerHtml}
-           ${pageHtml}
-           ${pricingHtml}
+           <h1>I did it!</h1>
+           <!-- Your footer here -->
         </body>
       </html>
     `);
